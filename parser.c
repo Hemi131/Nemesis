c901f77138e599d52ef4a59a52d7d88ac7f284ce
Filip Nehéz <filip.nehez@gmail.com>
2024-12-03 09:16:51 +0100

    Add stack and matrix data structures with basic operations

--- parser.c
+++ parser.c
@@ -2,3 +2,4 @@
 #include <stdlib.h>
 
 #include "parser.h"
+
---
+++ simplex.c
@@ -0,0 +1,1 @@
+//https://math.libretexts.org/Bookshelves/Applied_Mathematics/Applied_Finite_Mathematics_(Sekhon_and_Bloom)/04%3A_Linear_Programming_The_Simplex_Method/4.02%3A_Maximization_By_The_Simplex_Method
